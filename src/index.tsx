7b8f1e2be923ff1d35799aabfa8f42bf66a39139
Magomed Alimkhanov <scrythepvp@gmail.com>
2023-12-01 21:40:41 +0100

    feat: add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-
+import { Router, Route, Routes } from "@solidjs/router";
 import "./index.css";
 import App from "./App";
+import Test from "./components/Test";
 
 const root = document.getElementById("root");
 
-render(() => <App />, root!);
+render(
+  () => (
+    <Router>
+      <Routes>
+        <Route path="/*" component={App} />
+        <Route path="/user" component={Test} />
+      </Routes>
+    </Router>
+  ),
+  root!,
+);
